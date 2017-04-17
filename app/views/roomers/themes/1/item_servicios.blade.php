83b9ed57033dd5f05367ee069879164d4e677e6c
Rolando Arias <rolando@skywebplus.com>
2017-04-17 17:57:03 -0600

    Date List Service

--- app/views/roomers/themes/1/item_servicios.blade.php
+++ app/views/roomers/themes/1/item_servicios.blade.php
@@ -38,7 +38,7 @@
 		</div>
 		<div class="row">
 			 @foreach($business as $cat)
-			<a style="margin-bottom: 30px;" href="{{url('roomer/servicio-item/')}}/{{$cat->id}}" class="col s12 m12 waves-effect waves-white">
+			<a style="margin-bottom: 30px;" href="{{url('roomer/servicios-detalle/')}}/{{$cat->id}}" class="col s12 m12 waves-effect waves-white">
 				<span style="font-size: 1.2rem; display: block; color: white; position: relative; background: rgba(38, 166, 154, 0.82); width: 100%; padding: 0 5px; top: 0; z-index: 2;" class="card-title truncate">{{BusinessLang::where('business_id',$cat->id)->first()->name}} </span>
 				<div class="card-image z-depth-2">
 					<img src="{{$cat->picture}}">
