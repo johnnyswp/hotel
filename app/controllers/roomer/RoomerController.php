5b32226475041e8a50895ddde1868a326a95b679
Rolando Arias <rolando@skywebplus.com>
2017-04-20 17:58:12 -0600

    Update Info List

--- app/controllers/roomer/RoomerController.php
+++ app/controllers/roomer/RoomerController.php
@@ -757,7 +757,65 @@ class RoomerController extends \BaseController {
 	}
 
 
-	public function getInfoist($id)
+	public function getInfoList()
+	{
+		$stay = Stay::find(Session::get('token_stay'));
+
+		#$lang_id = LanguageHotel::find(Session::get('lang_id'))->language_id;
+		$lang_id = Session::get('lang_id');
+		
+		$hotel = Hotel::find($stay->hotel_id);
+		$lang = Language::find($lang_id);
+		 
+		$categories = DB::table('category_menu')
+				   	->Join('names_category_menu', 'names_category_menu.category_menu_id', '=', 'category_menu.id')
+				   	->where('hotel_id','=',$stay->hotel_id)
+				   	->where('category_menu.state','=',1)
+				   	->where('language_id','=',$lang_id)
+				   	->select('category_menu.id as category_id',
+					   		'names_category_menu.name as category_name',
+					   		'category_menu.picture as category_picture')
+ 					->orderBy('category_menu.order','ASC')
+				   	->get();
+		$exchange = Exchanges::find($hotel->exchange_id)->symbol;
+		
+		$phones =  DB::table('phones')
+				   	->Join('name_phones', 'name_phones.phone_id', '=', 'phones.id')
+				   	->where('phones.hotel_id','=',$stay->hotel_id)
+				   	->where('name_phones.language_id','=',$lang_id)
+				   	->where('phones.state','=',1)
+				   	->select('name_phones.id as phones_id',
+					   		'name_phones.name as phones_name',
+					   		'phones.number as phones_number')
+ 					->orderBy('phones.order','ASC')
+				   	->get();
+		
+		$today = Carbon::today();
+		$stay_token = Carbon::parse($stay->closing_date);		 
+	 
+		if($stay_token == $today){			
+			$max = "true";
+		}else{
+			$max = 1;	
+		}
+		 
+
+		$services = InfoPlace::where('hotel_id',$stay->hotel_id)->orderBy('infoOrder','ASC')->get();
+	 
+		$template = $hotel->theme; 
+		
+		return View::make("roomers.themes.$template.list_info")
+			->withHotel($hotel)
+			 ->withExchange($exchange)
+			->withCategories($categories)
+			->withPhones($phones)
+			->withMax($max) 
+			->withStay($stay)
+			->withLang($lang)
+			->withInfos($services);			
+	}	
+
+	public function getInfolistItem($id)
 	{
 
 		$service_id =$id;
@@ -803,11 +861,11 @@ class RoomerController extends \BaseController {
 		}
 		 
 
-		$business = Business::where('hotel_id',$stay->hotel_id)->where('service_id',$service_id)->orderBy('businessOrder','ASC')->get();
-		 
+		$business = InfoPlace::find($service_id);
+	 
 		$template = $hotel->theme; 
 		
-		return View::make("roomers.themes.$template.list_servicios")
+		return View::make("roomers.themes.$template.item_info")
 			->withHotel($hotel)
 			 ->withExchange($exchange)
 			->withCategories($categories)
